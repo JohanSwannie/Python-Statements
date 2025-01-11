02f6fe3c5d786480c95b136832a264cca535bbae
Johan Swan <jswannieman@gmail.com>
2025-01-12 07:15:29 +1300

    Commit pythonFor PY Component

---
+++ pythonFor.py
@@ -0,0 +1,60 @@
+
+# * FOR LOOP - EXAMPLE 1
+
+def forOne(a, b):
+    for i in range(a, b):
+        print(i)
+        
+forOne(3, 8)
+
+# * FOR LOOP - EXAMPLE 2
+
+def forTwo(list1):
+    for element in list1:
+        print(element)
+        underline = ''
+        for i in range(len(element)):
+            underline += "-"
+        print(underline)
+    
+list1 = ["Car", 'Bicycle', "Helicopter", "Bus", "Train", "Plane"]
+
+forTwo(list1)
+
+# * FOR LOOP - EXAMPLE 3
+
+def forThree(cars):
+    for car in cars:
+        if car == 'Nissan':
+            break
+        print(car)
+        
+cars = ["BMW", "Mercedes Benz", "Toyota", "Renault", "Volvo", "Nissan", "Ford", "Suzuki"]
+
+forThree(cars)
+
+# * FOR LOOP - EXAMPLE 4
+
+def forFour(cars2):
+    for car2 in cars2:
+        if car2 == 'Nissan':
+            continue
+        print(car2)
+        
+cars2 = ["BMW", "Mercedes Benz", "Toyota", "Renault", "Volvo", "Nissan", "Ford", "Suzuki"]
+
+forFour(cars2)
+            
+# * FOR LOOP - EXAMPLE 5
+
+def forFive(grp1, grp2):
+    for g1 in grp1:
+        for g2 in grp2:
+            print(g1, g2)
+    
+    
+
+group1 = ['Cobol', 'JavaScript', 'Python']
+group2 = ['Programming Language', 'Business Driven', 'Excellent']
+
+forFive(group1, group2)
