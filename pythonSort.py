ceca35c672a637320aba2ae344785bc549e479a1
Johan Swan <jswannieman@gmail.com>
2025-07-19 13:58:34 +1200

    Commit pythonSort  Pyhon Component

---
+++ pythonSort.py
@@ -0,0 +1,35 @@
+
+
+# * EXAMPLE 1 - SORT NUMBERS ASCENDING
+
+list1 = [13, 19, 10, 19, 4, 28, 33]
+
+list1.sort()
+
+print('list1 sorted = ', list1)
+
+# * EXAMPLE 2 - SORT NUMBERS DESCENDING
+
+list2 = [13, 19, 10, 19, 4, 28, 33]
+
+list2.sort(reverse=True)
+
+print('list2 sorted = ', list2)
+
+# * EXAMPLE 3 - SORT ALPHABETIC ASCENDING
+
+list3 = ["Gert", "Jan", "Koos", "Danie", "Hannes", "Zorro"]
+
+list3.sort()
+
+print('list3 sorted = ', list3)
+
+# * EXAMPLE 4 - SORT ALPHABETIC DESCENDING
+
+list4 = ["Gert", "Jan", "Koos", "Danie", "Hannes", "Zorro"]
+
+list4.sort(reverse=True)
+
+print('list4 sorted = ', list4)
+
+
