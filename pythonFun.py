fba942b9a7103f1daa3ee322e651ff5a29ab1471
Johan Swan <jswannieman@gmail.com>
2025-08-03 09:43:03 +1200

    Update  pythonFun Python Component

--- pythonFun.py
+++ pythonFun.py
@@ -1,87 +1,118 @@
 
-# * Fun Tricks 1 - Create a second list with data types from another list
+# # * Fun Tricks 1 - Create a second list with data types from another list
 
-list = [12, 'Panda', 13, None, 17, True, 'Joe']         
+# list = [12, 'Panda', 13, None, 17, True, 'Joe']         
 
-list2 = ["String" if isinstance(x, str) else "Boolean" if isinstance(x, bool) else "Number" if isinstance(x, int) else x for x in list]
+# list2 = ["String" if isinstance(x, str) else "Boolean" if isinstance(x, bool) else "Number" if isinstance(x, int) else x for x in list]
 
-print(list2)
+# print(list2)
 
-# * Fun Tricks 2 - Merge Dictionaries
+# # * Fun Tricks 2 - Merge Dictionaries
 
-j = {'Gary': 16, "Mary": 13, "Luke": 21, "Eugene": 19}
-k = {"Carl": 22, "Freddy": 24, "Gary": 20, "Hank": 21, "Mary": 23, "Luke": 20}
-l = {"Carl": 30, "Gary": 28, "Eugene": 33}
+# j = {'Gary': 16, "Mary": 13, "Luke": 21, "Eugene": 19}
+# k = {"Carl": 22, "Freddy": 24, "Gary": 20, "Hank": 21, "Mary": 23, "Luke": 20}
+# l = {"Carl": 30, "Gary": 28, "Eugene": 33}
 
-m = {**j, **k, **l}
-print(m)
+# m = {**j, **k, **l}
+# print(m)
 
-# * Fun Tricks 3 - Iterate with enumerate() instead of range(len())
+# # * Fun Tricks 3 - Iterate with enumerate() instead of range(len())
 
-# * Old Way
+# # * Old Way
 
-numberList = [21, 51, 16, 9, 78, 5]
+# numberList = [21, 51, 16, 9, 78, 5]
 
-for i in range(len(numberList)):
-    if numberList[i] > 50:
-        numberList[i] = "Clear Winner"
+# for i in range(len(numberList)):
+#     if numberList[i] > 50:
+#         numberList[i] = "Clear Winner"
         
-print(numberList)
+# print(numberList)
 
-# * Better Way
+# # * Better Way
 
-numberList2 = [10, 15, 76, 4, 18, 55]
+# numberList2 = [10, 15, 76, 4, 18, 55]
 
-for index1, number in enumerate(numberList2):
-    if number > 50:
-        numberList2[index1] = "Clear Winner"
+# for index1, number in enumerate(numberList2):
+#     if number > 50:
+#         numberList2[index1] = "Clear Winner"
 
-print(numberList2)
+# print(numberList2)
 
-# * Fun Tricks 4 - Use list comprehension instead of for loops
+# # * Fun Tricks 4 - Use list comprehension instead of for loops
 
-# * Old Way
+# # * Old Way
 
-evenNumbers = []
-for x in range(1, 11):
-    evenNumbers.append(x + x)
+# evenNumbers = []
+# for x in range(1, 11):
+#     evenNumbers.append(x + x)
 
-print(evenNumbers)
+# print(evenNumbers)
 
-# * Better Way
+# # * Better Way
 
-evenNumbers2 = [x + x for x in range(1, 11)]
+# evenNumbers2 = [x + x for x in range(1, 11)]
 
-print(evenNumbers2)
+# print(evenNumbers2)
 
-# * Fun Tricks 5 - Sort complex iterables
+# # * Fun Tricks 5 - Sort complex iterables
 
-personDict = [{"name": "Jacky", "age": 29}, 
-              {"name": "Luke", "age": 19}, 
-              {"name": "Carlos", "age": 22},
-              {"name": "Benjamin", "age": 23}
-        ]
-personDict_Sorted = sorted(personDict, key=lambda x: x["age"])
+# personDict = [{"name": "Jacky", "age": 29}, 
+#               {"name": "Luke", "age": 19}, 
+#               {"name": "Carlos", "age": 22},
+#               {"name": "Benjamin", "age": 23}
+#         ]
+# personDict_Sorted = sorted(personDict, key=lambda x: x["age"])
 
-print(personDict_Sorted)
+# print(personDict_Sorted)
 
-# * Fun Tricks 6 - Create sets from lists to eliminate duplicates
+# # * Fun Tricks 6 - Create sets from lists to eliminate duplicates
 
-list3 = ['Cat', 14, True, "Dog", 28, 14, "Cat", False, 21, True]
+# list3 = ['Cat', 14, True, "Dog", 28, 14, "Cat", False, 21, True]
 
-set1 = set(list3)
+# set1 = set(list3)
 
-print(set1)
+# print(set1)
 
-# * Fun Tricks 7 - Summing after creating a list
+# # * Fun Tricks 7 - Summing after creating a list
 
-k = [i for i in range(5)]
-print(sum(k))
+# k = [i for i in range(5)]
+# print(sum(k))
 
-# * Fun Tricks 8 - Reverse a string by using slicing
+# # * Fun Tricks 8 - Reverse a string by using slicing
 
-string1 = "They went up the hill"
+# string1 = "They went up the hill"
 
-reversedString1 = string1[:: -1]
+# reversedString1 = string1[:: -1]
 
-print(reversedString1)
+# print(reversedString1)
+
+# * Fun Tricks 9 - Check if a string is a Palindrome
+
+# * 1 - Compare end to end
+
+string = 'racecar'
+
+x, y = 0, len(string) - 1
+
+print(x, y)
+
+isPally = True
+
+while x < y:
+    if string[x] != string[y]:
+        isPally = False
+        break
+    x += 1
+    y -= 1
+    
+print(f'The string {string} is a Palindrome --> {isPally}')
+
+# * 2 - Slice Method
+
+if string == string[:: -1]:
+    print(f'The string {string} is a Palindrome')
+else:
+    print(f'The string {string} is not a Palindrome')
+    
+
+
