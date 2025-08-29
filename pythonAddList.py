0e28852db770427481944af6eece5757de0d7ce7
Johan Swan <jswannieman@gmail.com>
2025-08-29 22:18:54 +1200

    Commit pythonAddList Python Component

---
+++ pythonAddList.py
@@ -0,0 +1,32 @@
+
+# * Add Items toa Python List
+
+# * EXAMPLE 1
+
+list1 = [15, 39, 47]
+
+list1.append(33)
+
+print(list1)
+
+# * EXAMPLE 2
+
+list1.insert(3, 12)
+
+print(list1)
+
+# * EXAMPLE 3
+
+list2 = [25, 7, 10]
+
+list1.extend(list2)
+
+print(list1)
+
+# * EXAMPLE 4
+
+list3 = [40, 11, 17]
+
+list1 = list1 + list3
+
+print(list1)
