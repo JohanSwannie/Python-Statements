7a0d11e43b0623ea353938b36968bf9cc8f0f073
Johan Swan <jswannieman@gmail.com>
2025-08-30 17:35:51 +1200

    Update  pythonOtherListMethods Python Component

--- pythonOtherListMethods.py
+++ pythonOtherListMethods.py
@@ -6,3 +6,27 @@ list1 = [12, 19, 10, 25, 12, 7, 9, 33, 12, 15, 22, 36, 15, 20, 19]
 count = list1.count(12)
 
 print(count)
+
+# * EXAMPLE 2 - Determine the index of a specified element in the list
+
+list2 = ["Banana", "Orange", "Pear", "Guava", "Peach", "Lemon", "Fig"]
+
+index1 = list2.index("Lemon")
+
+print(index1)
+
+# * EXAMPLE 3 - Reverse the order of the list
+
+list3 = ['a', 'b', 'c', 'd', 'e']
+
+list3.reverse()
+
+print(list3)
+
+# * EXAMPLE 4 - Sort the list
+
+list4 = [19, 7, 13, 1, 10, 15, 5, 20, 29, 9, 4]
+
+list4.sort()
+
+print(list4)
