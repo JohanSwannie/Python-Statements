f63cfcb121d9f331388396b36004b76d4fd06239
Johan Swan <jswannieman@gmail.com>
2025-08-30 17:27:49 +1200

    Commit pythonOtherListMethods Python Component

---
+++ pythonOtherListMethods.py
@@ -0,0 +1,8 @@
+
+# * EXAMPLE 1 - Count how many times a specified element appears in the list
+
+list1 = [12, 19, 10, 25, 12, 7, 9, 33, 12, 15, 22, 36, 15, 20, 19]
+
+count = list1.count(12)
+
+print(count)
