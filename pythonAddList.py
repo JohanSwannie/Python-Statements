a2ec3b233078ffd9b472bde572a992d8169b5af0
Johan Swan <jswannieman@gmail.com>
2025-08-30 17:20:49 +1200

    Update pythonAddList Python Component

--- pythonAddList.py
+++ pythonAddList.py
@@ -30,3 +30,12 @@ list3 = [40, 11, 17]
 list1 = list1 + list3
 
 print(list1)
+
+# * EXAMPLE 5 - Copy the list
+
+list4 = ['Potatoes', 'Tomatoes', 'Lettuce']
+
+list5 = list4.copy()
+
+print(isinstance(list5, list))
+print(list5)
